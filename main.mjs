09d4abab727526899200b17492fce7ed77e9302b
xxmebyte <xxme3037@gmail.com>
2026-03-12 16:25:32 +0900

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -31,7 +31,7 @@ try {
     await page.locator('#user_password').fill(process.env.PASSWORD)
     await page.locator('text=ログインする').click()
     await page.waitForNavigation({ waitUntil: 'networkidle2' })
-    await page.locator('#campaignModalForFreeUsers .modal__close').click({ timeout: 10000 });
+    await page.locator('#campaignModalForFreeUsers .modal__close').click()
     await page.locator('a[href^="/xapanel/xvps/server/detail?id="]').click()
     await page.locator('text=更新する').click()
     await page.locator('text=引き続き無料VPSの利用を継続する').click()
